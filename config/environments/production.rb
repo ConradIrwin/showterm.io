80c9b41bb1a6458861c7f8960c3a2d9d63620f08
Conrad Irwin <conrad.irwin@gmail.com>
2012-10-14 11:56:52 -0700

    Don't compress stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ ShowtermIo::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
