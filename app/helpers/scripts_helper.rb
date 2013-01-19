27da13a9120768c20c9e0a454dee212fed9990e7
Conrad Irwin <conrad.irwin@gmail.com>
2013-01-19 03:02:25 -0800

    Encode JSON properly

--- app/helpers/scripts_helper.rb
+++ app/helpers/scripts_helper.rb
@@ -1,2 +1,15 @@
 module ScriptsHelper
+
+  # Output JSON suitable for putting in a <script> tag.
+  #
+  # The "/" character is escaped to avoid XSS of the form:
+  #   val == "foo</script><script>alert('gotcha')"
+  #
+  # "\u2028" and "\u2029" are characters that are valid as literals in JSON,
+  # but not in javascript: http://timelessrepo.com/json-isnt-a-javascript-subset
+  def raw_json(val)
+    raw(val.to_json.gsub("\u002f", "\\u002f")
+                   .gsub("\u2028", "\\u2028")
+                   .gsub("\u2029", "\\u2029"))
+  end
 end
