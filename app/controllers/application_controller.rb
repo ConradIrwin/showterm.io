9977325e699fc22357adc1196568ade4d2ce930d
Conrad Irwin <conrad.irwin@gmail.com>
2013-01-20 17:15:30 -0800

    Redirect away from showterm.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :redirect_to_io
+
+  def redirect_to_io
+    if request.base_url == "http://showterm.com"
+      redirect_to request.url.sub(".com", ".io")
+    end
+  end
 end
