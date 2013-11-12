50e186ac3c59bf8b2c878ba04fea2313e99613bd
Conrad Irwin <conrad.irwin@gmail.com>
2013-11-11 19:15:40 -0500

    No initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module ShowtermIo
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.assets.initialize_on_precompile = false
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
